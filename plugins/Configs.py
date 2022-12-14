46eac280b1870abf6d2d6361ee72ff1febe9129b
crosa <15132844856@163.com>
2022-12-14 13:51:33 +0800

    test

--- plugins/Configs.py
+++ plugins/Configs.py
@@ -2,4 +2,4 @@ import os
 default_font_path=os.getcwd()+"\\awesomebot\\plugins\\font\\fzzxhk.ttf"
 pixiv_preview_path=os.getcwd()+"\\awesomebot\\plugins\\pixiv\\preview"
 pixiv_path=os.getcwd()+"\\awesomebot\\plugins\\pixiv\\new"
-my_cookie="42279487_ELVc6XnA65su8WLT7tdNN8MKNE5qbLaH"
+my_cookie="your cookie"
